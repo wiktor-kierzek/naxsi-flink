f18f27c2894d1af6050ac588fbb9c1bf688ba6d6
Wiktor Kierzek <wiktor.kierzek@currency-one.com>
2017-09-09 10:59:36 +0200

    fwfweF

--- src/main/java/com/example/NaxsiFlink.java
+++ src/main/java/com/example/NaxsiFlink.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.ResultTypeQueryable;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.shaded.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
@@ -15,6 +24,7 @@ import org.apache.flink.streaming.connectors.rabbitmq.RMQSource;
 import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
+import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -25,15 +35,20 @@ import java.util.Map;
 /**
  * Created by _sn on 07.09.2017.
  */
+@Slf4j
 public class NaxsiFlink {
 
     public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        run();
+    }
 
-        env.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
+    public static boolean run() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        final RMQConnectionConfig rabbitMqConfig = new RMQConnectionConfig.Builder()
-            .setUri("amqp://flink:uGPxSmAWRuu5AtSx@51.255.199.51:5672").build();
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE);
+
+        RMQConnectionConfig rabbitMqConfig = new RMQConnectionConfig.Builder()
+                .setUri("amqp://flink:uGPxSmAWRuu5AtSx@51.255.199.51:5672").build();
 
         Map<String, String> elasticConfig = new HashMap<>();
         elasticConfig.put("cluster.name", "my-cluster-name");
@@ -42,26 +57,32 @@ public class NaxsiFlink {
         List<InetSocketAddress> transportAddresses = new ArrayList<>();
         transportAddresses.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 9300));
 
-        final SplitStream<Tuple> input = env
-            .addSource(new RMQSource<>(
-                rabbitMqConfig,
-                "naxsi",
-                true,
-                new SimpleStringSchema()
-            )).setParallelism(1)
-            .map(new ParseLogLine()).setParallelism(2)
-            .split((OutputSelector<Tuple>) value -> Lists.newArrayList(value.getField(0).toString()));
+        TypeInformation<Tuple2<String, String>> tpinf = new TypeHint<Tuple2<String, String>>(){}.getTypeInfo();
 
-        final DataStream<Tuple> ex = input.select("exlog");
+        DataStreamSink<NaxsiTuple> input = env
+                .addSource(new RMQSource<>(
+                        rabbitMqConfig,
+                        "naxsi",
+                        false,
+                        new SimpleStringSchema()
+                ), TypeInformation.of(String.class)).setParallelism(1).returns(String.class)
+                .map(new ParseLogLine())//.setParallelism(2).returns(new TupleTypeInfo(Tuple2.class))
+        .print();
+               // .split((OutputSelector<NaxsiTuple>) value -> Lists.newArrayList(value.log));
 
-        final DataStream<Tuple> fmt = input.select("fmt")
-            .join(ex)
-            .where(new GetHashForTuple()).equalTo(new GetHashForTuple())
-            .window(TumblingEventTimeWindows.of(Time.seconds(20)))
-            .apply((JoinFunction<Tuple, Tuple, Tuple>) (o, o2) -> {
-                return null; // TODO: Do join
-            });
-
+        //DataStream<NaxsiTuple> ex = input.select("exlog");
+/*
+        DataStream<NaxsiTuple> fmt = input.select("fmt")
+                .join(ex)
+                .where(new GetHashForTuple()).equalTo(new GetHashForTuple())
+                .window(TumblingEventTimeWindows.of(Time.seconds(20)))
+                .apply((JoinFunction<NaxsiTuple, NaxsiTuple, NaxsiTuple>) (o, o2) -> {
+                    log.info("LOG");
+                    log.info(o.message);
+                    log.info(o2.message);
+                    return new NaxsiTuple(o.log, o2.message); // TODO: Do join
+                });
+*/
 
         /** TODO:
          * - add Elastic sink
@@ -69,14 +90,24 @@ public class NaxsiFlink {
          * https://ci.apache.org/projects/flink/flink-docs-release-1.3/dev/connectors/elasticsearch.html
          */
 
+        env.execute("Naxsi example");
+
+        return true;
+    }
+
+    public static class GetHashForTuple implements KeySelector<NaxsiTuple, String> {
+
+        public String getKey(NaxsiTuple tuple) throws Exception {
+            log.info(tuple.message);
+            //calculate hash
+            return tuple.message;
+        }
 
     }
 
-    private static class GetHashForTuple implements KeySelector<Tuple, String>  {
-
-        public String getKey(Tuple tuple) throws Exception {
-            //calculate hash
-            return null;
-        }
+    @AllArgsConstructor
+    public static class NaxsiTuple implements Serializable {
+        private String log;
+        private String message;
     }
 }
--- src/main/java/com/example/ParseLogLine.java
+++ src/main/java/com/example/ParseLogLine.java
@@ -1,14 +1,20 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
 
 /**
  * Created by _sn on 07.09.2017.
  */
-public class ParseLogLine implements MapFunction<String, Tuple> {
+@Slf4j
+public class ParseLogLine implements MapFunction<String, NaxsiFlink.NaxsiTuple> {
 
-    public Tuple map(String s) throws Exception {
-        return null;
+    public NaxsiFlink.NaxsiTuple map(String s) throws Exception {
+        log.info("---------------- PARSER ----------");
+        log.info(s);
+
+        return new NaxsiFlink.NaxsiTuple(s.contains("fmt")?"fmt":"exlog", s);
     }
 }
